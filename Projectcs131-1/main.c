c89c402c964950fced449e6ceb6ba7727a7aa082
sonminhtran1997 <sonminhtran1997@gmail.com>
2017-01-05 23:46:33 -0800

    In progress

---
+++ Projectcs131-1/main.c
@@ -0,0 +1,60 @@
+//-------------------------------------------------------------------------------------------
+//	File: 
+//
+// Functions:
+//
+//-------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#define CENTS_IN_TWENTYDOLLARS 2000
+#define CENTS_IN_TENDOLLARS 1000
+#define CENTS_IN_FIVEDOLLARS 500
+#define CENTS_IN_ONEDOLLARS 100
+//-------------------------------------------------------------------------------------------
+//	Function:
+//
+// Title:
+//
+//	Description: 
+// 
+//
+// Programmer: 
+// 
+// Date:
+// Version: 
+//
+//	Environment:
+//                     Hardware:
+//
+//                      Software:
+//
+//	Input:
+//
+//	Output: 
+//
+//	Parameters:
+// 
+//
+// Returns:
+// 
+// 
+//	Called By:
+// 
+//	Calls: 
+//
+//	History Log:
+//
+// 
+//----------------------------------------------------------------------------
+
+
+int main(void)
+{
+	double purchaseAmount;
+	double moneyTendered;
+	printf("Welcome to Change Counter by Son Tran!\n");
+	printf("Please enter the total amount of purchase:");
+	purchaseAmount = scanf();
+	getchar();
+	return EXIT_SUCCESS;
+}
