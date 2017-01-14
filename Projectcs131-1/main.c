91a867c0ee9b55bd521897bb5fe063f8602e1900
sonminhtran1997 <sonminhtran1997@gmail.com>
2017-01-14 01:36:17 -0800

    Working on verifying input

--- Projectcs131-1/main.c
+++ Projectcs131-1/main.c
@@ -6,10 +6,14 @@
 //-------------------------------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define CENTS_IN_TWENTYDOLLARS 2000
 #define CENTS_IN_TENDOLLARS 1000
 #define CENTS_IN_FIVEDOLLARS 500
 #define CENTS_IN_ONEDOLLARS 100
+#define ZERO 0;
+#define ONE 1;
+#define TOOBIG 9999999999; 
 //-------------------------------------------------------------------------------------------
 //	Function:
 //
@@ -53,9 +57,18 @@ int main(void)
 	double purchaseAmount = 0;
 	double moneyTendered = 0;
 	double change = 0;
+	_Bool invalidPur = false;
+	_Bool invaliddTend = true;
+	int scanPur = 0;
 	printf("Welcome to Change Counter by Son Tran!\n");
 	printf("Please enter the total amount of purchase: $");
-	scanf("%lf", &purchaseAmount);
+	scanPur = scanf("%lf", &purchaseAmount);
+	while (getchar() != '\n');
+	while (invalidPur)
+	{
+		//printf("Invalid t, total amount of purchase should be number from 0 to 9999999999, please enter the ")
+	}
+	invalidPur = scanPur != ONE & purchaseAmount > TOOBIG;
 	printf("$%.2lf\n", purchaseAmount);
 	printf("Please enter amount of money tendered: $");
 	scanf("%lf", &moneyTendered);
